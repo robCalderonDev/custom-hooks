2d8378864fab9a16b7bac39941e3d30fd4770ab9
Robert Calderon <Robert.Calderon@ocean.ibm.com>
2022-11-15 17:30:44 -0300

    primer commit

---
+++ useTodos.js
@@ -0,0 +1,53 @@
+import React, { useReducer, useEffect, useState } from 'react'
+import { TodoReducer } from '../08-useReducer/TodoReducer';
+const initialState = [
+    // {
+    //     id: new Date().getTime(),
+    //     description: 'Recolectar la piedra del ama',
+    //     done: false
+    // },
+
+];
+const init = () => {
+    return JSON.parse(localStorage.getItem('todos') || [])
+}
+const useTodos = () => {
+
+
+    const [todos, dispatch] = useReducer(TodoReducer, initialState, init)
+
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos))
+
+
+    }, [todos])
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: '[TODO] add todo',
+            payload: todo
+        }
+        dispatch(action)
+    }
+    const handleDeleteTodo = (id) => {
+        dispatch({
+            type: '[TODO] remove todo',
+            payload: id
+        })
+    }
+    const handleToggleTodo = (id) => {
+        dispatch({
+            type: '[TODO] toggle todo',
+            payload: id
+        })
+    }
+
+
+    return {
+        todos, handleNewTodo, handleDeleteTodo, handleToggleTodo, todosCount: todos.length, pendingTodosCount: todos.filter(todo => !todo.done).length
+    }
+}
+export { useTodos }
+
+
+
